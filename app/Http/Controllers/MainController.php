5e4fcdccc63c9e72687eea10453d9f5bf5b6b71c
Edmir Cardoso <edmircrds@gmail.com>
2025-07-17 17:29:14 -0100

    Relation One to One and One To Many

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,74 +2,102 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
+use App\Models\Client;
 
 class MainController extends Controller
 {
-    public function index()
-    {
-        //devolvendo todos os dados de uma tabela SELECT * FROM clients
-        //$this->showRawData($clients);
-        //$clients = DB::table('clients')->get();
-        //$this->showDataTable($clients);
 
-        //apresentar num array associativo
-        //$clients = DB::table('clients')->get()->toArray();
-
-
-        //apresentar num array de arrays associativo
-        // $result = DB::table('products')->get()->map(function($item){
-        //     return (array) $item;
-        // });
-
-        //apresentar os dados a partir dos resultados
-        // $products = DB::table('products')->get();
-        // foreach ($products as $product){
-        //     echo $product->product_name . '<br>';
-
-        // }
-        //obter apenas colunas
-        // $products = DB::table('products')->get(['product_name', 'price']);
-
-        //pluck - obter de forma simples uma dados de coluna expecifico
-        // $results = DB::table('products')->pluck('product_name');
-
-        //devolver a primeira e ultimolinha de um resultado
-        //$results = DB::table('products')->get()->first();
-        //$results = DB::table('products')->get()->last();
-
-        //SELECT * from products Where id =10
-        $results = DB::table('products')->find(10);
-         $this->showRawData($results);
+    public function index(){
 
     }
+
+     public function OneToOne(){
+
+        //buscar telefone de um cliente
+        // $client1 = Client::find(12)->phone;
+        // echo "Telefone do cliente ID:" . $client1->client_id . ": " .$client1->phone_number;
+
+        //todos os dados de cliente e o telefone dele
+        // $client2 = Client::find(12);
+        // $phone = $client2->phone->phone_number;
+        // echo "<br>";
+        // echo "Nome do cliente: " . $client2->client_name . "<br>";
+        // echo "Telefone do cliente: " . $phone;
+        // echo '<hr>';
+
+        //se queseres buscar um conjunto de clientes e seus telefones
+        $clients = Client::with('phone')->get();
+        foreach($clients as $client){
+            echo '<br>';
+            echo "Nome de cliente: ".$client->client_name. "- Telefone: " .$client->phone->phone_number;
+        }
+
+    }
+
+    public function OneToMany(){
+       //todos os dados de cliente e o telefone dele
+        $client1 = Client::find(10);
+        $phones = $client1->phones;
+        echo "<br>";
+        // echo "Nome do cliente: " . $client1->client_name . "<br>";
+        // echo "Telefone do cliente: <br>";
+        // foreach($phones as $phone){
+        //     echo $phone->phone_number . "<br>";
+        // }
+
+          //se queseres buscar um conjunto de clientes e seus telefones
+         //se queseres buscar um conjunto de clientes e seus telefones
+        $clients = Client::with('phone')->get();
+        foreach($clients as $client){
+            echo '<br>';
+            echo "Nome de cliente: ".$client->client_name. "<br>";
+            echo "Telefone: <br> ";
+            foreach($client->phones as $phone){
+                echo $phone -> phone_number. "<br>";
+
+        }
+        echo '<hr>';
+
+    }
+}
+
+
+
 
     private function showRawData($data)
     {
-        echo '<pre>';
-        print_r($data);
-        echo '</pre>';
+        echo 'Eloquement relation';
     }
 
-    private function showDataTable($data)
+    private function ArrayOfObject($data)
     {
-        echo '<table border="1"';
-        // header
-        echo '<tr>';
-        foreach ($data[0] as $key => $value) {
-            echo '<th>' . $key . '</th>';
+        $temp = [];
+        foreach($data as $key => $value)
+        {
+            $tmp[] = (object) $value;
         }
-        echo '<tr>';
-        // body
-        foreach ($data as $row) {
-            echo '<tr>';
-            foreach ($row as $key => $value) {
-                echo '<td>' . $value . '</th>';
-            }
-        }
+        return $tmp;
 
-        echo '<tr>';
-        echo '</table>';
     }
+
+    // private function showDataTable($data)
+    // {
+    //     echo '<table border="1"';
+    //     // header
+    //     echo '<tr>';
+    //     foreach ($data[0] as $key => $value) {
+    //         echo '<th>' . $key . '</th>';
+    //     }
+    //     echo '<tr>';
+    //     // body
+    //     foreach ($data as $row) {
+    //         echo '<tr>';
+    //         foreach ($row as $key => $value) {
+    //             echo '<td>' . $value . '</th>';
+    //         }
+    //     }
+
+    //     echo '<tr>';
+    //     echo '</table>';
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\MainController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index']);
+Route::get('/one_to_many', [MainController::class, 'OneToMany']);
+
+
+// Route::get('/teste', function(){
+//     $products = Product::all();
+//     echo '<pre>';
+//     print_r($products->toArray());
+// });
